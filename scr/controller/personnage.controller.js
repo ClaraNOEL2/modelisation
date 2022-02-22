43edd13bbc3c9c3bb761cf60a1dbc424e5af58d9
clara.noel <clara.noel@eemi.com>
2022-02-22 17:28:50 +0100

    first commit

---
+++ scr/controller/personnage.controller.js
@@ -0,0 +1,7 @@
+class Personnage {
+    constructor(id, nom, sexe) {
+        this.id = id;
+        this.nom = nom;
+        this.sexe = sexe;
+    }
+}
---
+++ scr/controller/stat.controller.js
@@ -0,0 +1,7 @@
+class Personnage {
+    constructor(xp, niveau, vie) {
+        this.xp = xp;
+        this.niveau = niveau;
+        this.vie = vie;
+    }
+}
