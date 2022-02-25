ea31904bdf49c04e21215956174fb6d814564437
clara.noel <clara.noel@eemi.com>
2022-02-25 12:00:15 +0100

    Ajout router

---
+++ routes/inventaire.router.js
@@ -0,0 +1,15 @@
+app.get('/inventaires', (req,res) => {
+    res.status(200).json(inventaires)
+})
+
+app.get('/inventaires/:alimentaire', (req,res) => {
+    const alimentaire = parseInt(req.params.alimentaire)
+    const invent = inventaires.find(invent => invent.alimentaire === alimentaire)
+    res.status(200).json(invent)
+})
+
+app.get('/inventaires/:medicament', (req,res) => {
+    const medicament = parseInt(req.params.medicament)
+    const invent = inventaires.find(invent => invent.medicament === medicament)
+    res.status(200).json(invent)
+})
--- scr/controller/personnage.controller.js
+++ scr/controller/personnage.controller.js
@@ -1,21 +1,7 @@
-app.get('/personnages', (req,res) => {
-    res.status(200).json(personnages)
-})
-
-app.get('/personnages/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-    const user = personnages.find(user => user.id === id)
-    res.status(200).json(user)
-})
-
-app.get('/personnages/:nom', (req,res) => {
-    const nom = parseInt(req.params.nom)
-    const user = personnages.find(user => user.nom === nom)
-    res.status(200).json(user)
-})
-
-app.get('/personnages/:sexe', (req,res) => {
-    const sexe = parseInt(req.params.sexe)
-    const user = personnages.find(user => user.sexe === sexe)
-    res.status(200).json(user)
-})
+class Personnage {
+    constructor(id, nom, sexe) {
+        this.id = id;
+        this.nom = nom;
+        this.sexe = sexe;
+    }
+}
