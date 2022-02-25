1f815169ff467d1cccbc644ddc38452e343bcef0
clara.noel <clara.noel@eemi.com>
2022-02-25 11:52:17 +0100

    Ajout controller

--- scr/controller/personnage.controller.js
+++ scr/controller/personnage.controller.js
@@ -7,3 +7,15 @@ app.get('/personnages/:id', (req,res) => {
     const user = personnages.find(user => user.id === id)
     res.status(200).json(user)
 })
+
+app.get('/personnages/:nom', (req,res) => {
+    const nom = parseInt(req.params.nom)
+    const user = personnages.find(user => user.nom === nom)
+    res.status(200).json(user)
+})
+
+app.get('/personnages/:sexe', (req,res) => {
+    const sexe = parseInt(req.params.sexe)
+    const user = personnages.find(user => user.sexe === sexe)
+    res.status(200).json(user)
+})
