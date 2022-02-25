9e1f3b9891c92596685e4e4e74fe0264f8fa3848
jacqueline.kahozi@eemi.com <kahozijacqueline@gmail.com>
2022-02-25 10:41:29 +0100

    modifier 25/02

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require('express')
+const app = express()
+const personnages = require('./personnages.json')
+
 app.get('/personnages', (req,res) => {
     res.status(200).json(personnages)
 })
@@ -6,4 +10,8 @@ app.get('/personnages/:id', (req,res) => {
     const id = parseInt(req.params.id)
     const user = personnages.find(user => user.id === id)
     res.status(200).json(user)
+})
+
+app.listen(4567, () => {
+    console.log("Serveur à l'écoute")
 })
--- scr/controller/stat.controller.js
+++
@@ -1,7 +0,0 @@
-class Personnage {
-    constructor(xp, niveau, vie) {
-        this.xp = xp;
-        this.niveau = niveau;
-        this.vie = vie;
-    }
-}
